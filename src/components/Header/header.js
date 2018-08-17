d430feed1ddab549eeef3d5236bdaac5b349ca53
moxdev <moxdev1@gmail.com>
2018-08-17 11:02:18 -0400

    added web animations api

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
 import Link from 'gatsby-link'
 import styled from 'styled-components'
 import Img from 'gatsby-image'
@@ -10,7 +11,8 @@ const HeaderWrapper = styled.div`
   margin-bottom: 1.45rem;
   overflow: hidden;
   position: relative;
-  height: 70vh;
+  height: ${({isHome}) => (isHome ? "70vh" : "20vh")}
+
   h1 {
     img {
       height: 80px;
@@ -26,42 +28,76 @@ const HeaderContainer = styled.div`
   position: relative;
 `
 
-const Header = ({ data }) => (
-  <HeaderWrapper>
-    <HeaderContainer>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none',
-          }}
-        >
-          <img src={logo} alt="logo" />
-        </Link>
-      </h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </HeaderContainer>
-    <Img
-      style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: '100%',
-        height: '100%',
-      }}
-      sizes={data.background.sizes}
-    />
-  </HeaderWrapper>
-)
+export default class Header extends Component {
+  componentDidUpdate = (prevProps, prevState) => {
 
-export default Header
+    const {location} = this.props;
+
+    if(location.pathname !== prevProps.location.pathname) {
+      if(this.props.location.pathname === '/') {
+        this.wrapper.animate([
+          { height: "20vh" },
+          { height: "70vh" }
+        ], {
+          duration: 300,
+          fill: "forwards",
+          easing: "cubic-bezier(0.86,0,0.07,1)",
+          iterations: 1
+        })
+      } else {
+        this.wrapper.animate([
+          { height: "70vh" },
+          { height: "20vh" }
+        ], {
+          duration: 300,
+          fill: "forwards",
+          easing: "cubic-bezier(0.86,0,0.07,1)",
+          iterations: 1
+        })
+      }
+    }
+  }
+
+  render() {
+    const { data, location } = this.props
+    return (
+      <HeaderWrapper
+        isHome={location.pathname === "/"}
+        ref={(wrapper) => this.wrapper  = ReactDOM.findDOMNode(wrapper)}>
+        <HeaderContainer>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              <img src={logo} alt="logo" />
+            </Link>
+          </h1>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </HeaderContainer>
+        <Img
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%',
+          }}
+          sizes={data.background.sizes}
+        />
+      </HeaderWrapper>
+    )
+  }
+}
